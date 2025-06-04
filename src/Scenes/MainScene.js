d48613fc49972d97527976d3caa1ed6f8a8742bf
ShivaRavinutala <152668909+ShivaRavinutala@users.noreply.github.com>
2025-06-04 14:47:10 -0700

    Update MainScene.js

--- src/Scenes/MainScene.js
+++ src/Scenes/MainScene.js
@@ -4,7 +4,11 @@ class MainScene extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.setPath("./assets/");
+        this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
     }
 
     create() {
